e9474d692c32ae054c17959d86c520d775f08cc9
EGC-SARLU <egcsarlusociete@gmail.com>
2026-06-28 12:00:15 +0000

    feat: logo dans AppBar + CGV + order success + splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -38,3 +38,30 @@ class EgcApp extends ConsumerWidget {
     );
   }
 }
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 2), () {
+      if (mounted) Navigator.of(context).pushReplacementNamed('/');
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: EgcColors.primary,
+      body: Center(child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Image.asset('assets/images/logo_boutikcredit.png', height: 100),
+        const SizedBox(height: 24),
+        const CircularProgressIndicator(color: Colors.white, strokeWidth: 2),
+      ])),
+    );
+  }
+}
