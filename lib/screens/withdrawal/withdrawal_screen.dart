6711f1862b102383ab2d64c0e3bbcfb31249a084
EGC-SARLU <egcsarlusociete@gmail.com>
2026-07-01 11:36:53 +0000

    feat: swipe supprimer historique retraits utilisateur

--- lib/screens/withdrawal/withdrawal_screen.dart
+++ lib/screens/withdrawal/withdrawal_screen.dart
@@ -1,4 +1,5 @@
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import '../../services/providers.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
