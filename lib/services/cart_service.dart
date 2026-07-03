6a705a34c1359d58eb10602d9a01ce167b6a1f33
EGC-SARLU <egcsarlusociete@gmail.com>
2026-07-03 19:01:59 +0000

    fix: guillemets clé panier corrigés

--- lib/services/cart_service.dart
+++ lib/services/cart_service.dart
@@ -4,7 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../models/article_model.dart';
 
 class CartService {
-  static String get _key => 'egc_cart_\${FirebaseAuth.instance.currentUser?.uid ?? 'guest'}';
+  static String get _key => 'egc_cart_\${FirebaseAuth.instance.currentUser?.uid ?? "guest"}';
   final List<CartItem> _items = [];
 
   List<CartItem> get items => List.unmodifiable(_items);
