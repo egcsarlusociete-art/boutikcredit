e1ebdb2dca101f7e8563aa91472a305b982731c4
EGC-SARLU <egcsarlusociete@gmail.com>
2026-07-04 12:20:43 +0000

    fix: import go_router order detail screen

--- lib/screens/orders/order_detail_screen.dart
+++ lib/screens/orders/order_detail_screen.dart
@@ -2,6 +2,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../models/order_model.dart';
 import '../../services/providers.dart';
